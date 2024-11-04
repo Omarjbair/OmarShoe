f867f874b714fd639195938a3dc41bc566ebdcfe
Omarjbair <omar.jbair.o@gmail.com>
2024-11-04 17:41:40 +0300

    First Upload

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({children,}: Readonly<{children: React.ReactN
                 <div className="bg-red-100 border-l-4 border-red-500 text-red-700 p-4">
                     <p className="font-semibold flex items-center">
                         <TriangleAlert className="mr-2" />
-                        Notice: You can access the dashboard because this is a learning project, you can't add, edit, or delete any thing in it.
+                        Notice: You can access the dashboard because this is a learning project, you cant add, edit, or delete any thing in it.
                     </p>
                 </div>
                 {children}
